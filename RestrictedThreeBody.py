1557cb382457b50a82731f85fc1e55ddfc5fc4cd
Elliot Scott <elliotscott99@hotmail.co.uk>
2020-11-18 13:57:04 +0000

    Added the coriolis force and fixed the potential

--- RestrictedThreeBody.py
+++ RestrictedThreeBody.py
@@ -5,20 +5,21 @@ import scipy.integrate as igr
 import matplotlib.animation as animation
 
 matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
-xmin = -6
+xmin = -5
 ymin = xmin
 xmax = -xmin
 ymax = -xmin
 sr = 4 
-m1 = 2
+m1 = 10
 m2 = 1
 sm = m1 + m2
-x1o = (m1 * sr) / sm
-x2o = (-m2 * sr) / sm
+x1o = (m2 * sr) / sm
+x2o = (-m1 * sr) / sm
 x, y = [],[]
 G = 0.01 #this effects the animation speed because I can't be bothered to tidy that up
 #Og = np.sqrt((G * sm)/ (sr**3))
 Og = 0
+Oga = 1
 fig, ax = plt.subplots()
 
 
@@ -38,6 +39,10 @@ def gradEffPot(xp, yp):
     dl = xmin / 5000
     return ((effPot(xp + dl,yp) - effPot(xp, yp))/dl, (effPot(xp,yp + dl) - effPot(xp, yp))/dl)
 
+def fx(u, t, k):
+    return (u[1], -gradEffPot(u[0], k[0])[0] + (Oga * k[1]))
+def fy(u, t, k):
+    return (u[1], -gradEffPot(u[0], k[0])[1] - (Oga * k[1]))
 def f(u, t):
     return (u[1], -gradEffPot(u[0, 0], u[0, 1]))
 
@@ -48,17 +53,21 @@ y.append(0)
 delta = 0.025
 xgrid = np.arange(xmin, xmax, delta)
 ygrid = np.arange(ymin, ymax, delta)
-contourvals = np.arange(-0.2, 0, delta/100)
+contourvals = np.arange(-0.1, 0, delta/25)
 X, Y = np.meshgrid(xgrid, ygrid)
 Z = effPot(X,Y)
+numParts = 5
 CS = ax.contour(X, Y, Z, contourvals, colors='k', alpha = 0.3)
-xpos = np.linspace(xmin, xmax, 50)
-ypos = np.linspace(ymin, ymax, 50)
-XPOS, YPOS = np.meshgrid(xpos, ypos)
-vx = np.zeros(50)
-vy = np.zeros(50)
-XVEL, YVEL = np.meshgrid(vx, vy)
-delt = 0.4
+XPOS = [4.0, -5.5, -2.3, -2, -2]
+YPOS = [0, 0, 0, 3.2, -3.2]
+#xpos = np.linspace(xmin, xmax, numParts)
+#ypos = np.linspace(ymin, ymax, numParts)
+#XPOS, YPOS = np.meshgrid(xpos, ypos)
+#XVEL = np.zeros(numParts)
+#YVEL = np.zeros(numParts)
+XVEL = np.random.uniform(low=-0.1, high=0.1, size=(numParts,))
+YVEL = np.random.uniform(low=-0.1, high=0.1, size=(numParts,))
+delt = 0.05
 sc2 = ax.scatter(XPOS, YPOS, s=5)
 
 def animate(i):
@@ -66,29 +75,21 @@ def animate(i):
     global YVEL
     global XPOS
     global YPOS
-    x.clear()
-    y.clear()
-    x.append(x1o * np.cos(Og * i))
-    x.append(x2o* np.cos(Og * i))
-    y.append(x1o * np.sin(Og * i))
-    y.append(x2o* np.sin(Og * i))
-    Z = effPot(X,Y)
     xp =XPOS + (XVEL * delt)
     yp =YPOS + (YVEL * delt)
-    grd = gradEffPot(XPOS,YPOS)
-    (xv, yv) = (XVEL - (grd[0] * delt), YVEL- (grd[1] * delt))
+    grd = gradEffPot(xp,yp)
+    (xv, yv) = (XVEL - (grd[0] * delt) - (Oga * delt * (-YVEL)), YVEL- (grd[1] * delt) - (Oga * delt * XVEL))
 
     XVEL = xv
     YVEL = yv
     XPOS = xp
     YPOS = yp
-    #sc3 = ax.scatter(xp, yp, s=1, c="r", marker="o")
-    sc2.set_offsets(np.c_[np.concatenate(XPOS),np.concatenate(YPOS)])
-    #CS = ax.contour(X, Y, Z, contourvals, colors='k')
+    #sc2.set_offsets(np.c_[np.concatenate(XPOS),np.concatenate(YPOS)])
+    sc2.set_offsets(np.c_[XPOS,YPOS])
     sc.set_offsets(np.c_[x,y])
     
 
 ani = animation.FuncAnimation(fig, animate, 
-                frames=2000, interval=1, repeat=True) 
+                frames=2000, interval=100, repeat=True) 
 
 plt.show()
